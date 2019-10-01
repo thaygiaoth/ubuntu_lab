96f3c63e073746287b599169380e1e2594cea430
thaygiaoth <thaygiaoth@gmail.com>
2019-10-01 09:20:03 +0700

    Minh họa chạy c sharp trên linux bằng mono

---
+++ hello.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Dela.Mono.csharp
+{
+   public class dot_net_on_linux
+   {
+      public static void Main(string[] args)
+      {
+
+	//\n là new line --> xuống đầu dòng mới
+	Console.WriteLine("\nLớp Linux DevOps I\n");
+
+	Console.WriteLine("Nhấn 1 phím bất kì để kết thúc"); 
+      
+        //Đọc 1 phím bất kì khi nhấn để thoát
+        Console.ReadKey(); 
+      }
+   } 
+}
